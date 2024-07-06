3116fa6f2141817cfe2788f5929a1632b2989b0a
raikankleberson <raikankleberson@gmail.com>
2024-07-06 15:18:18 -0300

    Inclusão do index,css,js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#passsword");
+const jobSelector = document.querySelector("#job");
+const messageTexturea = document.querySelector("#message");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Verifica se o nome está vazio
+});
